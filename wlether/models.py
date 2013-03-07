d85c1a43c00f23b2b990548bf67a3c82698f73ba
Theso <teu@hp.trwy>
2013-03-07 08:14:40 +0400

    https://docs.djangoproject.com/en/1.5/intro/tutorial02/  Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     Committer: Theso <teu@hp.trwy>
    
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   .emacs.desktop
    	modified:   .emacs.desktop.lock
    	modified:   cuzco/settings.py
    	modified:   cuzco/settings.pyc
    	modified:   cuzco/urls.py
    	modified:   cuzco/urls.pyc
    	modified:   wlether/models.py
    	modified:   wlether/models.pyc
    
     Untracked files:
       (use "git add <file>..." to include in what will be committed)
    
    	.gitignore
    	templates/
    	wlether/admin.py

--- wlether/models.py
+++ wlether/models.py
@@ -7,16 +7,25 @@ class Scan(models.Model):
     adapter = models.CharField(max_length=200)
     tool = models.CharField(max_length=200)
     scan_time = models.DateTimeField('time scaned')
+
     def __unicode__(self):
-        return self.scan_time
+        return unicode(self.scan_time)
 
 
-class APoint (models.Model):                       #
+class APoint (models.Model):
     scan = models.ForeignKey(Scan)
-    bssid = models.CharField(max_length=50)        #bssid VARCHAR(50),
-    frequency = models.CharField(max_length=50)    #frequency VARCHAR(50),
-    signal_level = models.CharField(max_length=50) #signal_level VARCHAR(50),
-    flags = models.CharField(max_length=50)        #flags VARCHAR(50),
-    ssid = models.CharField(max_length=50)         #ssid VARCHAR(50),
+    bssid = models.CharField(max_length=50)
+    frequency = models.CharField(max_length=50)
+    signal_level = models.CharField(max_length=50)
+    flags = models.CharField(max_length=50)
+    ssid = models.CharField(max_length=50)
+
+    def was_it_channel_6(self):
+        return self.frequency == '2437'
+
+    was_it_channel_6.admin_order_field = 'frequency'
+    was_it_channel_6.boolean = True
+    was_it_channel_6.short_description = 'Channel 6?'
+
     def __unicode__(self):
         return self.ssid
