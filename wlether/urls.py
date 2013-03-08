d7667922ad6c56a6b11d84b1b60dca7e3b246d32
Theso <teu@hp.trwy>
2013-03-08 08:36:13 +0400

    until https://docs.djangoproject.com/en/1.5/intro/tutorial04/#use-generic-views-less-code-is-better  Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     Committer: Theso <teu@hp.trwy>
    
     On branch master
     Changes to be committed:
       (use "git reset HEAD <file>..." to unstage)
    
    	modified:   .emacs.desktop
    	deleted:    .emacs.desktop.lock
    	modified:   .gitignore
    	deleted:    cuzco/__init__.pyc
    	deleted:    cuzco/settings.pyc
    	deleted:    cuzco/urls.pyc
    	deleted:    cuzco/wsgi.pyc
    	deleted:    wlether/__init__.pyc
    	deleted:    wlether/models.pyc
    	modified:   wlether/templates/wlether/detail.html
    	new file:   wlether/templates/wlether/details.html
    	modified:   wlether/urls.py
    	modified:   wlether/views.py
    
     Untracked files:
       (use "git add <file>..." to include in what will be committed)
    
    	hp.c
    	hp.head
    	pa.c
    	pa.head

--- wlether/urls.py
+++ wlether/urls.py
@@ -7,8 +7,10 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     # ex: /wlether/5/
     url(r'^(?P<scan_id>\d+)/$', views.detail, name='detail'),
+    # ex: /wlether/5/5/details/
+    url(r'^(?P<scan_id>\d+)/(?P<apoint_id>\d+)/details/$', views.details, name='details'),
     # ex: /wlether/5/results/
-    url(r'^(?P<scan_id>\d+)/results/$', views.results, name='results'),
-    # ex: /wlether/5/vote/
-    url(r'^(?P<scan_id>\d+)/vote/$', views.vote, name='vote'),
+    url(r'^(?P<scan_id>\d+)/(?P<apoint_id>\d+)/results/$', views.results, name='results'),
+    # ex: /wlether/5/5/collect/
+    url(r'^(?P<scan_id>\d+)/(?P<apoint_id>\d+)/collect/$', views.collect, name='collect'),
 )
--- wlether/views.py
+++ wlether/views.py
@@ -1,10 +1,12 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 # from django.template import Context, loader
 from django.shortcuts import render, get_object_or_404
 # from django.http import Http404
+from django.core.urlresolvers import reverse
+from datetime import datetime
 
-from wlether.models import Scan
+from wlether.models import Scan, APoint
 
 import base64
 
@@ -37,16 +39,54 @@ def index(request):
     context = {'latest_scan_list': latest_scan_list}       # =
     return render(request, 'wlether/index.html', context)
 
+@basic_http_auth
 def detail(request, scan_id):
-    # try:                                     # from django.http import Http404
-    #     scan = Scan.objects.get(pk=scan_id)  # .
-    # except Scan.DoesNotExist:                # .
-    #     raise Http404                        # .
-    scan = get_object_or_404(Scan, pk=scan_id) # =
+    # try:                                           # from django.http import Http404
+    #     scan = Scan.objects.get(pk=scan_id)        # .
+    # except Scan.DoesNotExist:                      # .
+    #     raise Http404                              # .
+    scan = get_object_or_404(Scan, pk=scan_id)          # =
     return render(request, 'wlether/detail.html', {'scan': scan})
 
-def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll %s." % poll_id)
+@basic_http_auth
+def details(request, scan_id, apoint_id):
+    apoint = APoint.objects.get(pk=apoint_id)
+    # template = loader.get_template('wlether/index.html') # from django.template import Context, loader
+    # context = Context({                                  # .
+    #     'latest_scan_list': latest_scan_list,            # .
+    # })                                                   # .
+    # return HttpResponse(template.render(context))        # .
+    context = {'scan_id': scan_id, 'apoint': apoint}       # =
+    return render(request, 'wlether/details.html', context)
 
-def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s." % poll_id)
+@basic_http_auth
+def results(request, scan_id, apoint_id):
+    return HttpResponse("You're looking at the results of poll %s" % scan_id)
+
+@basic_http_auth
+def collect(request, scan_id, apoint_id):
+    if scan_id == '0':
+        s = Scan() 
+        s.chassis = request.POST['chassis']
+        s.adapter = request.POST['adapter']
+        s.tool = request.POST['tool']
+        #s.time = request.POST['time']
+        s.scan_time = datetime.strptime(request.POST['time'], '%Y-%m-%d %H:%M:%S')
+        #s.time = datetime.now()
+        s.save()
+        ap = s.apoint_set.create()
+        scan_id = s.id
+        apoint_id = ap.id
+    else:
+        ap = get_object_or_404(APoint, pk=apoint_id)
+
+    ap.bssid = request.POST['bssid']
+    ap.frequency = request.POST['frequency']
+    ap.signal_level = request.POST['signal_level']
+    ap.flags = request.POST['flags']
+    ap.ssid = request.POST['ssid']
+    ap.save()
+    # Always return an HttpResponseRedirect after successfully dealing
+    # with POST data. This prevents data from being posted twice if a
+    # user hits the Back button.
+    return HttpResponseRedirect(reverse('wlether:results', args=(scan_id, apoint_id,)))
