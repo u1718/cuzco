cb5ab46ad7e3541a8771fec7fbac7b8f2d9ad6d3
drizla <drizla@inbox.ru>
2017-01-19 16:52:11 +0300

    reopen move old proj to orphan branch

--- cuzco/urls.py
+++ cuzco/urls.py
@@ -1,18 +1,26 @@
-from django.conf.urls import patterns, include, url
+"""cuzco URL Configuration
 
-# Uncomment the next two lines to enable the admin:
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url, include
 from django.contrib import admin
-admin.autodiscover()
+from django.contrib.auth import views as auth_views
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'cuzco.views.home', name='home'),
-    # url(r'^cuzco/', include('cuzco.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^wlether/', include('wlether.urls', namespace="wlether")),
-)
+urlpatterns = [
+    url(r'^weather/', include('weather.urls')),
+    url(r'^admin/', admin.site.urls),
+    #url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^accounts/login/$', auth_views.login, {'template_name': 'admin/login.html'}),
+    ]
---
+++ weather/views.py
@@ -0,0 +1,100 @@
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import Http404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils import timezone
+
+import requests
+import json
+
+from .models import City, OWM, OWMForecast
+
+class CityView(LoginRequiredMixin, generic.ListView):
+    #login_url = ''
+    #redirect_field_name = ''
+    #template_name = 'weather/city_list.html'
+    #context_object_name = 'city_list'
+
+    def get_queryset(self):
+        """Return requested cities."""
+        return City.objects.order_by('name')
+
+class OWMView(generic.DetailView):
+    model = City
+    #template_name = 'weather/city_detail.html'
+
+class OWMForecastView(generic.DetailView):
+    model = OWM
+    #template_name = 'weather/owm_detail.html'
+
+def cron(request):
+    username = request.GET['username']
+    password = request.GET['password']
+    user = authenticate(username=username, password=password)
+    if user is not None:
+        login(request, user)
+        #http://api.openweathermap.org/data/2.5/forecast/city?id=498698&APPID=775670b8133c08911511535c6b1dfbdf
+        url = 'http://api.openweathermap.org/data/2.5/forecast/city'
+        params = {
+            'id':r'498698',
+            'appid':r'775670b8133c08911511535c6b1dfbdf'
+        }
+
+        resp = requests.get(url, params = params)
+        
+        # with open('./owm.data') as f:
+        #     w = f.read()
+
+        # class Resp:
+        #     def __init__(self, t):
+        #         self.text = t
+                
+        # resp = Resp(w)
+        
+        if resp is not None:
+            
+            pj = json.loads(resp.text)
+            context = {
+                'root': pj.keys(),
+                'city': pj['city'],
+                'list': pj['list'],
+            }
+                
+            owm = OWM()
+            
+            owm.iden = pj['city']['id']
+            owm.name = pj['city']['name']
+            owm.coord_lon = pj['city']['coord']['lon']
+            owm.coord_lat = pj['city']['coord']['lat']
+            owm.country = pj['city']['country']
+            owm.population = pj['city']['population']
+            owm.sys_population = pj['city']['sys']['population']
+
+            owm.cod = pj['cod']
+            owm.message = pj['message']
+            owm.cnt = pj['cnt']
+
+            owm.city = City.objects.get(name=pj['city']['name'])
+            owm.req_date = timezone.now()
+
+            owm.save()
+
+            for fcs in pj['list']:
+                owm_forecast = OWMForecast()
+                owm_forecast.owm = owm
+                owm_forecast.forecast_text = fcs
+                owm_forecast.save()
+            
+            
+            return render(request, 'weather/cronsucc.html', context)
+        else:
+            return render(request, 'weather/cronfail.html', {'error': 'req fail'})
+    else:
+        return render(request, 'weather/cronfail.html', {'error': 'auth fail', 'u': username, 'p': password})
+
+
+
+
