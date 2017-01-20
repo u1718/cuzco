9c2d9a8f42570571d3f7edf8e0ad3e30595d0da9
drizla <drizla@inbox.ru>
2017-01-20 15:43:21 +0300

    request scheduler mockup

--- weather/models.py
+++ weather/models.py
@@ -7,9 +7,10 @@ from django.utils import timezone
 class City(models.Model):
     name = models.CharField(max_length=100)
     ds_owm = models.URLField()
+    turn = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.name
+        return '{} {}'.format(self.name, self.turn)
 
 class OWM(models.Model):
     city = models.ForeignKey(City, on_delete=models.CASCADE)
@@ -26,7 +27,7 @@ class OWM(models.Model):
     cnt = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return '{}{:_>10}{:_>10}{:_>10}{:_>10}'.format(str(self.req_date), self.cod, self.message, self.cnt, self.name)
 
     def was_requested_recently(self):
         return self.req_date >= timezone.now() - datetime.timedelta(days=1)
--- weather/views.py
+++ weather/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.http import Http404
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils import timezone
@@ -36,64 +36,113 @@ def cron(request):
     user = authenticate(username=username, password=password)
     if user is not None:
         login(request, user)
-        #http://api.openweathermap.org/data/2.5/forecast/city?id=498698&APPID=775670b8133c08911511535c6b1dfbdf
-        url = 'http://api.openweathermap.org/data/2.5/forecast/city'
-        params = {
-            'id':r'498698',
-            'appid':r'775670b8133c08911511535c6b1dfbdf'
-        }
-
-        resp = requests.get(url, params = params)
         
+        context = dict()
+        
+        c = City.objects.filter(turn=True).first()
+        if c is None:
+            City.objects.all().update(turn=True)
+            c = City.objects.filter(turn=True).first()
+
+        c.turn = False
+        c.save()
+            
+        resp = requests.get(c.ds_owm)
+
+        # #http://api.openweathermap.org/data/2.5/forecast/city?id=498698&APPID=775670b8133c08911511535c6b1dfbdf
+        # url = 'http://api.openweathermap.org/data/2.5/forecast/city'
+        # params = {
+        #     'id':r'498698',
+        #     'appid':r'775670b8133c08911511535c6b1dfbdf'
+        # }
+
+        # resp = requests.get(url, params = params)
+
         # with open('./owm.data') as f:
         #     w = f.read()
 
         # class Resp:
         #     def __init__(self, t):
         #         self.text = t
-                
+
         # resp = Resp(w)
-        
+
         if resp is not None:
-            
+
             pj = json.loads(resp.text)
+
+            if pj['cod'] == '0':
+                context = {
+                    'fail': '',
+                    'error': pj['message']
+                }
+                owm = OWM()
+
+                owm.message = pj['message']
+
+                owm.city = c
+                owm.req_date = timezone.now()
+
+                owm.save()
+
+            else:
+                context = {
+                    'ok': '',
+                    'root': pj.keys(),
+                    'city': pj['city'],
+                    'list': pj['list'],
+                }
+
+                owm = OWM()
+
+                owm.iden = pj['city']['id']
+                owm.name = pj['city']['name']
+                owm.coord_lon = pj['city']['coord']['lon']
+                owm.coord_lat = pj['city']['coord']['lat']
+                owm.country = pj['city']['country']
+                owm.population = pj['city']['population']
+                owm.sys_population = pj['city']['sys']['population']
+
+                owm.cod = pj['cod']
+                owm.message = pj['message']
+                owm.cnt = pj['cnt']
+
+                owm.city = c
+                owm.req_date = timezone.now()
+
+                owm.save()
+
+                for fcs in pj['list']:
+                    owm_forecast = OWMForecast()
+
+                    owm_forecast.owm = owm
+                    owm_forecast.forecast_text = fcs
+                    owm_forecast.save()
+
+        else:
             context = {
-                'root': pj.keys(),
-                'city': pj['city'],
-                'list': pj['list'],
+                'fail': '',
+                'error': 'req fail'
             }
-                
+
             owm = OWM()
-            
-            owm.iden = pj['city']['id']
-            owm.name = pj['city']['name']
-            owm.coord_lon = pj['city']['coord']['lon']
-            owm.coord_lat = pj['city']['coord']['lat']
-            owm.country = pj['city']['country']
-            owm.population = pj['city']['population']
-            owm.sys_population = pj['city']['sys']['population']
 
-            owm.cod = pj['cod']
-            owm.message = pj['message']
-            owm.cnt = pj['cnt']
+            owm.message = 'No Response'
 
-            owm.city = City.objects.get(name=pj['city']['name'])
+            owm.city = c
             owm.req_date = timezone.now()
 
             owm.save()
 
-            for fcs in pj['list']:
-                owm_forecast = OWMForecast()
-                owm_forecast.owm = owm
-                owm_forecast.forecast_text = fcs
-                owm_forecast.save()
-            
-            
-            return render(request, 'weather/cronsucc.html', context)
-        else:
-            return render(request, 'weather/cronfail.html', {'error': 'req fail'})
+        logout(request)
+        
     else:
-        return render(request, 'weather/cronfail.html', {'error': 'auth fail', 'u': username, 'p': password})
+        context = {
+            'fail': '',
+            'error': 'auth fail'
+        }
+        
+    return render(request, 'weather/cron.html', context)
 
 
 
